1d0319be60e3a9c1a6533efc8bb7feea922dcb44
fridgeBaron <fridgeBaron@gmail.com>
2020-02-17 01:33:33 -0800

    Add project files.

---
+++ Utils/outPut.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+using UnityEngine;
+using System.Diagnostics;
+
+
+namespace Utils
+{
+    
+    static class outPut
+    {
+        static outPut()
+        {
+
+        }
+        //[ConditionalAttribute("DEBUG")]
+        public static void Log(String message)
+        {
+            Verse.Log.Message(message);
+        }
+
+    }
+}
